0955519c81314c8a96bb28241efda8d00e69936c
gabrieldacosta <gdc.business.2019@gmail.com>
2022-01-29 13:13:11 +0100

    Mise à jour Vue.py

--- R&D/Vue.py
+++ R&D/Vue.py
@@ -12,6 +12,9 @@ CHOIX_MENU_PRINCIPAL = {1 : 'Créer un nouveau tournoi',
 # nombres de tours (Round) par tournoi
 NOMBRE_DE_TOURS = 4
 
+# nombre de joueurs par tournoi
+NOMBRE_DE_JOUEURS = 8
+
 class Vue:
     '''Classe qui gère l'interface/menu du programme.'''
 
@@ -100,12 +103,107 @@ Créer un nouveau Tournoi
         # Description
         nouveau_tournoi['description'] = input('Rensignez les remarques générales du directeur du tournoi : ')
 
-
         return nouveau_tournoi
 
+
+    def ajouter_huit_joueurs(self) -> List[Dict[str, str]]:
+        '''AJout des informations de huit joueurs dans une liste de dictionnaires à destination du Controller.'''
+
+        # Initialisation
+        # liste retournée contenant les informations des huit joueurs
+        nouveaux_joueurs = []
+        # dictionnaire type contenant les informations d'un joueur
+        information_nouveau_joueur = {
+            'nom_de_famille' : '',
+            'prénom': '',
+            'date_de_naissance': '',
+            'sexe': '',
+            'classement' : ''
+        }
+
+        # Affichage de l'entête
+        affichage_menu_ajouter_huit_joueurs = '''
+==============================
+Ajouter ''' + str(NOMBRE_DE_JOUEURS) + ''' joueurs
+==============================
+'''
+        print(affichage_menu_ajouter_huit_joueurs)
+
+        # Boucle sur les huits joueurs
+        for joueur in range(1,9):
+            # ajout du dictionnaire type à la liste de nouveaux joueurs
+            nouveaux_joueurs.append(information_nouveau_joueur)
+
+            # Affichage de l'entête pour chaque nouveau joueur
+            affichage_nouveau_joueur = '''
+    Renseigner les informations du joueur n°''' + str(joueur) + '''
+    ==========================================
+            '''
+            print(affichage_nouveau_joueur)
+
+            # Boucle pour définir le nom de famille
+            while(True):
+                try:
+                    nouveaux_joueurs[joueur-1]['nom_de_famille'] = str(input('\nRenseignez le Nom de famille du joueur n°' + str(joueur) +' : '))
+                    if len(nouveaux_joueurs[joueur-1]['nom_de_famille']) > 0:
+                        break
+                    else:
+                        raise Exception
+                except:
+                    print('Veuillez renseigner un Nom de famille contenant au moins un charactère.')
+
+            # Boucle pour définir le prénom
+            while(True):
+                try:
+                    nouveaux_joueurs[joueur-1]['prénom'] = str(input('\nRenseignez le Prénom du joueur n°' + str(joueur) +' : '))
+                    if len(nouveaux_joueurs[joueur-1]['prénom']) > 0:
+                        break
+                    else:
+                        raise Exception
+                except:
+                    print('Veuillez renseigner un Prénom contenant au moins un charactère.')
+
+            # Boucle pour définir la date de naissance
+            while(True):
+                try:
+                    entree_date = str(input('\nRenseignez la Date de naissance du joueur n°' + str(joueur) +' au format "jj/mm/aaaa" : '))
+                    jour_test, mois_test, annee_test = entree_date.split('/')
+                    if len(annee_test) != 4:
+                        raise Exception
+                    else:
+                        nouveaux_joueurs[joueur-1]['date_de_naissance'] = date(int(annee_test), int(mois_test), int(jour_test))
+                        break
+                except:
+                    print('Veuillez renseigner une date valide au format "jj/mm/aaaa".')
+
+            # Boucle pour définir le sexe
+            while(True):
+                try:
+                    nouveaux_joueurs[joueur-1]['sexe'] = str(input('\nRenseignez le sexe du joueur n°' + str(joueur) +' (M/F) : '))
+                    if nouveaux_joueurs[joueur-1]['sexe'] == 'M':
+                        break
+                    elif nouveaux_joueurs[joueur - 1]['sexe'] == 'F':
+                        break
+                    else:
+                        raise Exception
+                except:
+                    print('Veuillez renseigner le sexe du joueur sous la forme "M" pour masculin ou "F" pour féminin.')
+
+            # Boucle pour définir le classement
+            while(True):
+                try:
+                    nouveaux_joueurs[joueur-1]['classement'] = int(input('\nRenseignez le classement du joueur n°' + str(joueur) +' : '))
+                    if nouveaux_joueurs[joueur-1]['classement'] > 0:
+                        break
+                    else:
+                        raise Exception
+                except:
+                    print('Veuillez renseigner un entier positif.')
+
+        return nouveaux_joueurs
 
 if __name__ == '__main__':
 
     initiation_vue = Vue()
-    resultat = initiation_vue.menu_creer_nouveau_tournoi()
+    resultat = initiation_vue.ajouter_huit_joueurs()
     print(resultat)
