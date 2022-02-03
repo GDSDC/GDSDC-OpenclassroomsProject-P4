19bb4c31a3beff0e035fdb32a03566a8c804c321
GDSDC <gdc.business.2019@gmail.com>
2022-02-03 17:55:43 +0100

    Mise à jour de Parse_Validate_Tools.py

--- Parse_Validate_Tools.py
+++ Parse_Validate_Tools.py
@@ -1,6 +1,10 @@
 from typing import Any, Dict, List, Tuple, Optional, Callable
 from datetime import date
 
+
+### Global constant
+MENU_LENGTH = 6 # -> Find a way to get the data from the length of dictionnary CHOIX_MENU_PRINCIPAL
+
 ### Functions for input parsing
 
 def parse_string_not_empty(user_input: str) -> Tuple[Optional[Any], bool, str]:
@@ -8,7 +12,7 @@ def parse_string_not_empty(user_input: str) -> Tuple[Optional[Any], bool, str]:
     if len(user_input) > 0:
         return user_input, True, ''
     else:
-        return None, False, 'String length must be > 0'
+        return None, False, 'Votre entrée doit contenir au moins 1 caractère.'
 
 def parse_int(user_input: str):
     ''' Function that verify the input is an int'''
@@ -16,7 +20,7 @@ def parse_int(user_input: str):
         res = int(user_input)
         return res, True, ''
     except ValueError:
-        return None, False, f'{user_input} is not a valid integer'
+        return None, False, f"{user_input} n'est pas un entier valide."
 
 
 def parse_date(user_input: str):
@@ -26,7 +30,7 @@ def parse_date(user_input: str):
         res = date(int(year), int(month), int(day))
         return res, True, ''
     except ValueError:
-        return None, False, f'{user_input} is not a valid date'
+        return None, False, f"{user_input} n'est pas un date valide."
 
 
 
@@ -38,7 +42,15 @@ def validate_date_format(parsed_date: date):
         res = parsed_date
         return res, True, ''
     else:
-        return None, False, 'Date should be in the format dd/mm/yyyy'
+        return None, False, 'La date doit être au format jj/mm/aaaa'
+
+def validate_integer_interval(parsed_int: int, interval: Tuple[int, int] = [1,MENU_LENGTH]):
+    '''Function that verify if the integer is in the interval'''
+    res = parsed_int
+    if (parsed_int >= interval[0]) and (parsed_int <= interval[1]):
+        return res, True, ''
+    else:
+        return None, False, f"{parsed_int} n'est pas compris entre {interval[0]} et {interval[1]}."
 
 def no_validation(user_input):
     '''Function that do no validation when this is not needed'''
@@ -50,7 +62,7 @@ def no_validation(user_input):
 
 def parse_and_validate(explanation: str,
                        parse: Callable[[str], Tuple[Optional[Any], bool, str]],
-                       validate: Callable[[Any], Tuple[Any, bool, str]]):
+                       validate: Callable[[Any], Tuple[Any, bool, str]] = no_validation):
     is_valid, error_message = False, None
     final_result = None
     while not is_valid:
@@ -66,7 +78,4 @@ def parse_and_validate(explanation: str,
 
 if __name__ == '__main__':
 
-    date_explanation = 'Renseignez un date : '
-    result = parse_and_validate(explanation=date_explanation, parse=parse_date,validate=validate_date_format)
-    result2 = parse_and_validate(explanation=date_explanation, parse=parse_date, validate=no_validation)
-    print(result2)
+    pass
