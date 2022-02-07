1f31ff0baf6f2d99b595cc8d37f2f9d70e7f7361
GDSDC <gdc.business.2019@gmail.com>
2022-02-07 10:12:01 +0100

    Mise à jour Controller.py

--- Controller.py
+++ Controller.py
@@ -11,9 +11,17 @@ class Controller:
 
     def creer_nouveau_tournoi(self):
         nouveau_tournoi = self.vue.menu_creer_nouveau_tournoi()
+        nouveau_tournoi = Model.Tournoi(**nouveau_tournoi)
         self.state.creer_nouveau_tournoi(nouveau_tournoi)
+
+    def ajouter_joueurs(self):
+        joueurs = self.vue.ajouter_huit_joueurs()
+        for indice_joueur in range(len(joueurs)):
+            joueurs[indice_joueur] = Model.Joueur(**joueurs[indice_joueur])
+        self.state.ajouter_joueurs(joueurs)
+
 
 if __name__ == '__main__':
 
     initiation_controller = Controller()
-    creer_nouveau_tournoi_test = initiation_controller.creer_nouveau_tournoi()
+    creer_nouveau_tournoi_test = initiation_controller.ajouter_joueurs()
