44aafbccd2df24521ab118c1d1ef5e75c068cd16
GDC-Business <gdc.business.2019@gmail.com>
2022-02-08 17:43:04 +0100

    test(main.py): add test_ajouter_joueurs function  to generate Joueurs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import controller
 
 initiation_controller = controller.Controller()
-initiation_controller.ajouter_joueurs()
+initiation_controller.test_ajouter_joueurs(nombre_joueur=3)
 resultat = initiation_controller.state.joueurs
 print(resultat)
