ddb6aff723a3301b190180a8e4f0cf22d9814ca8
GDSDC <gdc.business.2019@gmail.com>
2022-02-17 11:47:48 +0100

    build(main) : testing app via main

--- main.py
+++ main.py
@@ -2,18 +2,8 @@ from core import controller
 
 
 init_controller = controller.Controller()
-init_controller.test_creer_nouveau_tournoi()
-init_controller.test_ajouter_joueurs(nombre_joueur=4)
+init_controller.creer_nouveau_tournoi()
+init_controller.ajouter_joueurs(nb_joueurs=2)
 init_controller.creer_nouveau_round()
 init_controller.generer_paires_joueurs()
 init_controller.entrer_scores()
-print(init_controller.state.actual_round.match_liste)
-
-# for match in init_controller.state.actual_round.match_liste:
-#     fields = list(match.__dict__.keys())
-#     # print(atr)
-#     # fields = match.__dataclass_fields__
-#     # print(fields)
-#     for result in fields:
-#         value = getattr(match, result)
-#         print(value.score)
