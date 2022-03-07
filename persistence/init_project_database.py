bf30f467b7d8735afcfe761c6e1a4e2b5fdcfde8
GDSDC <gdc.business.2019@gmail.com>
2022-03-07 17:32:05 +0100

    build(init_project_database) : spliting save function into save_players and save_tournaments

--- persistence/init_project_database.py
+++ persistence/init_project_database.py
@@ -1,64 +1,103 @@
-import os, sys
+import os
 
 from tinydb import TinyDB, Query, table
 from core.model import Joueur, State, Tournoi
 from tests.test_script import state1, state4
 from core import sorters
-from typing import Dict, List
+from typing import Dict, List, Optional
+from functools import wraps
+
+_DB_PATH = 'resources/db.json'
+
+DB: Optional[TinyDB] = None
 
 
-def init_database():
+def open_database(path: str = _DB_PATH) -> TinyDB:
+    print("Opening connection to DB")
+    return TinyDB(path)
+
+
+def init_database(db: TinyDB):
     """Function to initialize database of the project"""
-    db = TinyDB('persistence/db.json')
+    print("Initializing DB tables")
     db.table('players')
-    db.table('tournament')
+    db.table('tournaments')
+    print("Done!")
 
 
 def clear_database():
     """Function to clear database"""
     try:
-        db_path = 'persistence/db.json'
-        os.remove(db_path)
+        print("Dropping DB")
+        os.remove(_DB_PATH)
     except FileNotFoundError:
         pass
-    init_database()
+    global DB
+    DB = open_database()
+    init_database(DB)
 
 
+def _ensure_db_is_loaded():
+    global DB
+    if DB is None:
+        DB = open_database()
+    else:
+        print("DB is already opened")
+
+
+def with_db(f):
+    @wraps(f)
+    def wrapper(**kargs):
+        _ensure_db_is_loaded()
+        return f(**kargs)
+
+    return wrapper
+
+
+@with_db
 def load_players() -> Dict[int, Joueur]:
     """Function to load players from database into actual state"""
 
     # Initalisation
     acteurs_from_db = {}
-    db = TinyDB('persistence/db.json')
     # Iteration over players in db
-    for element in db.table('players'):
+    for element in DB.table('players'):
         acteurs_from_db[element.doc_id] = Joueur.from_json(element)
 
     return acteurs_from_db
 
 
-def save(state: State):
-    """Function to save actual state to database"""
+def save_players(state: State):
+    """Function to save players to database"""
 
-    # Initialisation
-    db = TinyDB('persistence/db.json')
-
-    # Save players
     for player_id, player_instance in state.acteurs.items():
-        db.table('players').insert(table.Document(player_instance.to_json(), doc_id=player_id))
+        DB.table('players').insert(table.Document(player_instance.to_json(), doc_id=player_id))
 
-    # Save tournaments
+
+def save_tournaments(state: State):
+    """Function to save tournaments to database"""
+
     tournaments = state.tournois
     if state.tournoi:
         tournaments.append(state.tournoi)
     tournaments.sort(key=sorters.tournament_chronological)
 
     for tournoi in tournaments:
-        db.table('tournaments').insert(tournoi.to_json())
+        DB.table('tournaments').insert(tournoi.to_json())
+
+@with_db
+def save(state: State):
+    """Function to save actual state to database"""
+
+    # Save players
+    save_players(state=state)
+
+    # Save tournaments
+    save_tournaments(state=state)
 
 
 if __name__ == '__main__':
     clear_database()
-    init_database()
+    init_database(db=DB)
     state_to_save = state4()
     save(state=state_to_save)
