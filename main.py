d33dd0a6952a59bfdec36247c147208ae818d049
GDSDC <gdc.business.2019@gmail.com>
2022-03-11 13:51:03 +0100

    fix(PEP8) : removing imports used before for tests and no more used

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 from core.controller import Controller
-from core.model import Tournoi, ControleDuTemps, Round, RoundName, Score
-from datetime import datetime, timedelta
-from tests.test_script import player_test
 
 init_controller = Controller()
 init_controller.load_database()
